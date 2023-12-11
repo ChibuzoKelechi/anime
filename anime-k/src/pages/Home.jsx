71a9ca28afa113dc80e57e41fe0dda94739561b2
Chibuzo Kelechi <narutokelechi@gmail.com>
2023-12-11 18:16:17 +0100

    Added files, sass and tailwind

---
+++ anime-k/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import '../public/styles/home.scss'
+
+const Homepage = () => {
+    return ( 
+        <section className="home">
+            <article className="hero grid place-items-center">
+                This is peakAnime
+            </article>
+        </section>
+     );
+}
+ 
+export default Homepage;
